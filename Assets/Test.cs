df6e09d85530ac2eb96601a7ea89fae2faac8f45
kohei <ondatyan@gmail.com>
2018-06-20 16:49:49 +0900

    Update Test.cs

--- Assets/Test.cs
+++ Assets/Test.cs
@@ -6,11 +6,10 @@ public class Test : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		
+		Debug.Log("Test");
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
 	}
 }
